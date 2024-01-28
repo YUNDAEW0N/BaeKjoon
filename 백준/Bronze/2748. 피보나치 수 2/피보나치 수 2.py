67e6eb46dcd7db83bf594ff4a35ef9bfa9236791
YUNDAEWON <155865459+YUNDAEW0N@users.noreply.github.com>
2024-01-28 14:43:46 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+
+N=int(input())
+
+
+def dp_fibo(n):
+    dp=[0]*(n+1)
+    dp[0],dp[1]=0,1
+
+    for i in range(2,n+1):
+        dp[i]=dp[i-2]+dp[i-1]
+
+    return dp[n]
+
+
+print(dp_fibo(N))
